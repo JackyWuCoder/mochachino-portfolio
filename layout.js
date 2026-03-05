190630d74d65abfcc6d7b0bf7e2875cf2ee1ff3e
Jacky Wu <JackyWuCoder@gmail.com>
2026-03-04 19:59:22 -0800

    feat: add "active" attribute to class when page loads

--- layout.js
+++ layout.js
@@ -2,6 +2,22 @@ async function loadComponent(id, file) {
   const res = await fetch(file);
   const text = await res.text();
   document.getElementById(id).innerHTML = text;
+
+  if (id === "navbar") {
+    setActiveLink();
+  }
+}
+
+function setActiveLink() {
+  const links = document.querySelectorAll(".navbar a"); // Finds all navbar links elements
+  const currentPath = window.location.pathname;
+
+  links.forEach((link) => {
+    const linkPath = new URL(link.href).pathname;
+    if (currentPath == linkPath) {
+      link.classList.add("active");
+    }
+  });
 }
 
 loadComponent("navbar", "/components/navbar.html");
