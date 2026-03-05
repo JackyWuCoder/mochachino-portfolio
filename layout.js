f1a8d76161149932f8bd6542e0f89d30c41b9856
Jacky Wu <JackyWuCoder@gmail.com>
2026-03-04 23:04:42 -0800

    fix(paths): add relative paths to navbar and footer

--- layout.js
+++ layout.js
@@ -20,5 +20,5 @@ function setActiveLink() {
   });
 }
 
-loadComponent("navbar", "/components/navbar.html");
-loadComponent("footer", "/components/footer.html");
+loadComponent("navbar", "./components/navbar.html");
+loadComponent("footer", "./components/footer.html");
